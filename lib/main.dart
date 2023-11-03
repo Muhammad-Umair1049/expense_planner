8327a3283d4043bc34928804588d3e7ad80578e7
Osaid-Khan <103985807+Osaid-Khan@users.noreply.github.com>
2023-11-03 15:09:20 +0500

    Added User's Profile screen(Dummy)..

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/Widgets/settings/me.dart';
 import 'package:flutter_complete_guide/widgets/auth_screens/sign_in.dart';
 import 'widgets/new_transaction.dart';
 import 'widgets/transaction_list.dart';
@@ -14,10 +15,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Personal Expenses',
       theme: ThemeData(primarySwatch: Colors.purple),
-      home: MyHomePage(
-        key: const Key('MyHomePage'), // Adding the named 'key' parameter here
-      ),
-      // home: SignIn(),
+      // home: MyHomePage(
+      //   key: const Key('MyHomePage'), // Adding the named 'key' parameter here
+      // ),
+      home: HelloScreen(),
     );
   }
 }
